2b039dfa6b62954b36387cbe40844c5356ab2b4e
lentera <89829920+lentera14@users.noreply.github.com>
2023-12-11 19:06:53 +0700

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "D:\\Kuliah\\Semester5\\ppb\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\Kuliah\\Semester5\\ppb\\codingan\\UAS\\contacts_app" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=D:\\Kuliah\\Semester5\\ppb\\flutter"
+  "PROJECT_DIR=D:\\Kuliah\\Semester5\\ppb\\codingan\\UAS\\contacts_app"
+  "FLUTTER_ROOT=D:\\Kuliah\\Semester5\\ppb\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\Kuliah\\Semester5\\ppb\\codingan\\UAS\\contacts_app\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\Kuliah\\Semester5\\ppb\\codingan\\UAS\\contacts_app"
+  "FLUTTER_TARGET=D:\\Kuliah\\Semester5\\ppb\\codingan\\UAS\\contacts_app\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC9iMjAxODNlMDQwOTYwOTRiY2MzN2Q5Y2RlMmE0Yjk2ZjVjYzY4NGNmLw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\Kuliah\\Semester5\\ppb\\codingan\\UAS\\contacts_app\\.dart_tool\\package_config.json"
+)
