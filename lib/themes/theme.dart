6e208be5cef5502aeed859798e324317452740db
agyhafidzah <89822868+agyhafidzah@users.noreply.github.com>
2023-12-11 21:43:46 +0700

    Add files via upload

---
+++ lib/themes/theme.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+ThemeData lightMode = ThemeData(
+  brightness: Brightness.light,
+  useMaterial3: true,
+  primaryColor: Color.fromRGBO(229, 183, 211, 1),
+);
+
+ThemeData darkMode = ThemeData(
+    brightness: Brightness.dark,
+    useMaterial3: true,
+    primaryColor: Color.fromRGBO(48, 23, 38, 1));
